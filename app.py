7b08ca93778ad0a07e07361ff53defff8bef7a21
mohd82640 <mohd82640@gmail.com>
2024-10-12 23:17:59 +0530

    added db

--- app.py
+++ app.py
@@ -2,13 +2,17 @@ from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String, Float
 import os
+from flask_marshmallow import Marshmallow
+
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' +  os.path.join(basedir, "planets.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+    os.path.join(basedir, "planets.db")
 
 
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
 
 @app.cli.command('db_create')
@@ -89,6 +93,13 @@ def url_variables(name: str, age: int):
         return jsonify(message="Welcome "+name)
 
 
+@app.route('/planets', methods=['GET'])
+def planets():
+    planets_list = Planet.query.all()
+    result = planets_schema.dump(planets_list)
+    return jsonify(result)
+
+
 # database models
 class User(db.Model):
     __tablename__ = 'users'
@@ -101,13 +112,31 @@ class User(db.Model):
 
 class Planet(db.Model):
     __tablename__ = "planets"
-    planet_id = id = Column(Integer, primary_key=True)
+    planet_id = Column(Integer, primary_key=True)
     planet_name = Column(String)
     planet_type = Column(String)
     home_star = Column(String)
     mass = Column(Float)
     radius = Column(Float)
     distance = Column(Float)
+
+
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'first_name', 'last_name', 'email', 'password')
+
+
+class PlanetSchema(ma.Schema):
+    class Meta:
+        fields = ('planet_id', 'planet_name', 'planet_type',
+                  'homestar', 'mass', 'radius', 'distance')
+
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+
+planet_schema = PlanetSchema()
+planets_schema = PlanetSchema(many=True)
 
 
 if __name__ == "__main__":
